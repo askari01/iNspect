0c08ba3fd7cd1ef8474d8fbae37986df7af5d0f9
Farrukh Askari <farrukh.askari01@gmail.com>
2017-05-12 11:09:17 +0500

    pinch to zoom in and Out pull to refresh frame set to view of device

--- iNspect/ViewController.m
+++ iNspect/ViewController.m
@@ -20,8 +20,11 @@
     NSURL *url = [NSURL URLWithString:@"https://inspect.tenderwiz.com"];//NSURL *url = [NSURL URLWithString:@"https://inspect.tenderwiz.com"];
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
     // Do any additional setup after loading the view, typically from a nib.
-    
+    [self addPullToRefreshToWebView];
+    self.webView.scalesPageToFit = YES;
+    _webView.frame = self.view.frame;
 }
+
 - (void)webViewDidStartLoad:(UIWebView *)webView
 {
     
@@ -57,7 +60,24 @@
     }
     else
         return YES;
+}
 
+- (void)addPullToRefreshToWebView{
+    UIColor *whiteColor = [UIColor whiteColor];
+    UIRefreshControl *refreshController = [UIRefreshControl new];
+    NSString *string = @"Pull down to refresh...";
+    NSDictionary *attributes = @{ NSForegroundColorAttributeName : whiteColor };
+    NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:string attributes:attributes];
+    refreshController.bounds = CGRectMake(0, 0, refreshController.bounds.size.width, refreshController.bounds.size.height);
+    refreshController.attributedTitle = attributedString;
+    [refreshController addTarget:self action:@selector(refreshWebView:) forControlEvents:UIControlEventValueChanged];
+    [refreshController setTintColor:whiteColor];
+    [self.webView.scrollView addSubview:refreshController];
+}
+
+- (void)refreshWebView:(UIRefreshControl*)refreshController{
+    [self.webView reload];
+    [refreshController endRefreshing];
 }
 
 @end
